e302fa94211ed4a89ac64a52adcaed7f928097a3
erripollo <pollone.enrico@gmail.com>
2021-06-08 12:56:17 +0200

    add dynamic search function

--- Milestone-1/assets/js/main.js
+++ Milestone-1/assets/js/main.js
@@ -4,26 +4,28 @@ const app = new Vue({
     data:{
         urlMovie: 'https://api.themoviedb.org/3/search/movie',
         apiKey: '99b48ac4b26b50b984fcdf28ea59ab53',
-        query: 'a',
+        query: '',
         moviesData: null
 
     },
 
     methods:{
 
-        
+        search(){
+            const fullUrlMovie = `${this.urlMovie}?api_key=${this.apiKey}&query=${this.query}`
+            console.log(fullUrlMovie);
+            axios
+            .get(fullUrlMovie)
+            .then(resp => {
+            console.log(resp.data.results);
+            this.moviesData = resp.data.results
+            })
+        }
 
     },
 
     mounted(){
-        const fullUrlMovie = `${this.urlMovie}?api_key=${this.apiKey}&query=${this.query}`
-        console.log(fullUrlMovie);
-        axios
-        .get(fullUrlMovie)
-        .then(resp => {
-            console.log(resp.data.results);
-            this.moviesData = resp.data.results
-        })
+        
 
     }
 
