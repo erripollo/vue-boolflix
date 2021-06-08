73b693d1105084e93726c3747da9bedfd014b55a
erripollo <pollone.enrico@gmail.com>
2021-06-08 17:06:53 +0200

    add flag for indicate the movie language

---
+++ Milestone-2/assets/js/main.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#app',
+
+    data:{
+        urlMovie: 'https://api.themoviedb.org/3/search/movie',
+        apiKey: '99b48ac4b26b50b984fcdf28ea59ab53',
+        query: '',
+        moviesData: []
+
+    },
+
+    methods:{
+
+        search(){
+            const fullUrlMovie = `${this.urlMovie}?api_key=${this.apiKey}&query=${this.query}`
+            console.log(fullUrlMovie);
+            axios
+            .get(fullUrlMovie)
+            .then(resp => {
+            console.log(resp.data.results);
+            this.moviesData = resp.data.results
+            })
+            .catch(e => {
+                console.error(e);
+            })
+        }
+
+    },
+
+    mounted(){
+        
+
+    }
+
+})
