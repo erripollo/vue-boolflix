afb40e19af1728d91cf22d4b17a0269e3c731882
erripollo <pollone.enrico@gmail.com>
2021-06-09 16:16:13 +0200

    add poster movies / tv show

---
+++ Milestone-3/assets/js/main.js
@@ -0,0 +1,62 @@
+const app = new Vue({
+    el: '#app',
+
+    data:{
+        urlMovie: 'https://api.themoviedb.org/3/search/movie',
+        urlTvShow: 'https://api.themoviedb.org/3/search/tv',
+        apiKey: '99b48ac4b26b50b984fcdf28ea59ab53',
+        query: '',
+        moviesData: null,
+        tvShowsData: null,
+        urlPoster:'https://image.tmdb.org/t/p/w342',
+
+    },
+
+    methods:{
+
+        search(){
+            const fullUrlMovie = `${this.urlMovie}?api_key=${this.apiKey}&query=${this.query}`
+            console.log(fullUrlMovie);
+            axios
+            .get(fullUrlMovie)
+            .then(resp => {
+            console.log(resp.data);
+            this.moviesData = resp.data
+            })
+            .catch(e => {
+                console.error(e);
+            })
+
+            const fullUrlTvShow = `${this.urlTvShow}?api_key=${this.apiKey}&query=${this.query}`
+            console.log(fullUrlTvShow);
+            axios
+            .get(fullUrlTvShow)
+            .then(resp => {
+                console.log(resp.data);
+                this.tvShowsData = resp.data
+            })
+            .catch(e =>{
+                console.error(e);
+            })
+        },
+
+       /*  flagsError(e){
+
+            if (e.target.src === 'https://www.countryflags.io/en/shiny/32.png') { 
+                e.target.src = "https://www.countryflags.io/gb/shiny/32.png" 
+            }
+            else if (e.target.src === 'https://www.countryflags.io/ja/shiny/32.png') {
+                e.target.src = "https://www.countryflags.io/jp/shiny/32.png" 
+            }
+            
+            
+        } */
+
+    },
+
+    mounted(){
+        
+
+    }
+
+})
