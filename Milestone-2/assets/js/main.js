2444a131d9af39562b53a8b569ccbb5f047870cc
erripollo <pollone.enrico@gmail.com>
2021-06-09 14:22:53 +0200

    event @error in flags img not work

--- Milestone-2/assets/js/main.js
+++ Milestone-2/assets/js/main.js
@@ -6,8 +6,8 @@ const app = new Vue({
         urlTvShow: 'https://api.themoviedb.org/3/search/tv',
         apiKey: '99b48ac4b26b50b984fcdf28ea59ab53',
         query: '',
-        moviesData: [],
-        tvShowsData: []
+        moviesData: null,
+        tvShowsData: null
 
     },
 
@@ -19,8 +19,8 @@ const app = new Vue({
             axios
             .get(fullUrlMovie)
             .then(resp => {
-            console.log(resp.data.results);
-            this.moviesData = resp.data.results
+            console.log(resp.data);
+            this.moviesData = resp.data
             })
             .catch(e => {
                 console.error(e);
@@ -31,13 +31,25 @@ const app = new Vue({
             axios
             .get(fullUrlTvShow)
             .then(resp => {
-                console.log(resp.data.results);
-                this.tvShowsData = resp.data.results
+                console.log(resp.data);
+                this.tvShowsData = resp.data
             })
             .catch(e =>{
                 console.error(e);
             })
-        }
+        },
+
+       /*  flagsError(e){
+
+            if (e.target.src === 'https://www.countryflags.io/en/shiny/32.png') { 
+                e.target.src = "https://www.countryflags.io/gb/shiny/32.png" 
+            }
+            else if (e.target.src === 'https://www.countryflags.io/ja/shiny/32.png') {
+                e.target.src = "https://www.countryflags.io/jp/shiny/32.png" 
+            }
+            
+            
+        } */
 
     },
 
