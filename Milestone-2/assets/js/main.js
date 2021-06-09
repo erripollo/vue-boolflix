3069ba290fdd0ca04043144f58248cdd0775419d
erripollo <pollone.enrico@gmail.com>
2021-06-09 09:01:44 +0200

    add TVshows in search, create a section for TVshows

--- Milestone-2/assets/js/main.js
+++ Milestone-2/assets/js/main.js
@@ -3,9 +3,11 @@ const app = new Vue({
 
     data:{
         urlMovie: 'https://api.themoviedb.org/3/search/movie',
+        urlTvShow: 'https://api.themoviedb.org/3/search/tv',
         apiKey: '99b48ac4b26b50b984fcdf28ea59ab53',
         query: '',
-        moviesData: []
+        moviesData: [],
+        tvShowsData: []
 
     },
 
@@ -21,6 +23,18 @@ const app = new Vue({
             this.moviesData = resp.data.results
             })
             .catch(e => {
+                console.error(e);
+            })
+
+            const fullUrlTvShow = `${this.urlTvShow}?api_key=${this.apiKey}&query=${this.query}`
+            console.log(fullUrlTvShow);
+            axios
+            .get(fullUrlTvShow)
+            .then(resp => {
+                console.log(resp.data.results);
+                this.tvShowsData = resp.data.results
+            })
+            .catch(e =>{
                 console.error(e);
             })
         }
